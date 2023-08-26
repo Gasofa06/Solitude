c081e0c2e8f8ebee00c2cc5c818bc2d5766f1429
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-08-27 01:46:05 +0200

    Constants

---
+++ site/javascript/constants/search_types.js
@@ -0,0 +1,16 @@
+export const SEARCH_TYPES = {
+    Title: {
+        id: 0,
+        placeholder: 'Search article by title (e.g. Conan Edogawa)',
+    },
+
+    Topic: {
+        id: 2,
+        placeholder: 'Search posts by topic (e.g. Anime; Music;)',
+    },
+
+    'Ref.': {
+        id: 1,
+        placeholder: 'Search article by reference (e.g. 286)',
+    },
+};
--- site/javascript/constants/server_paths.js
+++ site/javascript/constants/server_paths.js
@@ -1,4 +1,7 @@
 export const API_URL = '/api';
+
 export const CHECK_URL = '/check';
+
 export const SETUP_URL = '/setup';
+
 export const QUERY_URL = '/query';
