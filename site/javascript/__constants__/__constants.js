6d462c31d472cc171fcccc19eabc3b4591a5a408
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-08-30 10:55:42 +0200

    CONSTANTS.

---
+++ site/javascript/__constants__/__constants.js
@@ -0,0 +1,3 @@
+export * from './server_paths.js';
+export * from './client_key.js';
+export * from './search_types.js';
---
+++ site/javascript/__constants__/search_types.js
@@ -0,0 +1,13 @@
+export const SEARCH_TYPES = {
+    Title: {
+        placeholder: 'Search article by title (e.g. Conan Edogawa)',
+    },
+
+    Topic: {
+        placeholder: 'Search posts by topic (e.g. Anime; Music;)',
+    },
+
+    'Ref.': {
+        placeholder: 'Search article by reference (e.g. 286)',
+    },
+};
