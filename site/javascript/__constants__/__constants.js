d58020c59cf7590470eb014d15afea5ec713ce27
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-08-31 18:09:40 +0200

    Constants.

--- site/javascript/__constants__/__constants.js
+++ site/javascript/__constants__/__constants.js
@@ -1,3 +1,4 @@
 export * from './server_paths.js';
 export * from './client_key.js';
 export * from './search_types.js';
+export * from './max_articles_in_page.js';
---
+++ site/javascript/__constants__/max_articles_in_page.js
@@ -0,0 +1,1 @@
+export const MAX_ARTICELS_IN_PAGE = 40;
