ed2b47285102af1dbc9cb2fadb7b0c135b1d9ddf
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-09-01 19:09:26 +0200

    Inverted index for searching articles with topics.

---
+++ site/javascript/server/data_setup/data_structures/inverted_index.js
@@ -0,0 +1,74 @@
+export class TopicsInvertedIndex {
+    constructor(_dict_title_information, _valid_topics) {
+        this.valid_topics = _valid_topics;
+        this.dict_topic_titles = {};
+
+        this.build_structure();
+        this.insert_data(_dict_title_information);
+    }
+
+    build_structure() {
+        for (let topic of this.valid_topics) {
+            this.dict_topic_titles[topic] = [];
+        }
+    }
+
+    add_article(_article_title, _arr_article_topics) {
+        for (let article_topic of _arr_article_topics) {
+            let contains_topic = !this.dict_topic_titles[article_topic];
+
+            if (contains_topic === true) {
+                console.log(
+                    'The article is associated with a topic does not exist.',
+                );
+
+                break;
+            }
+
+            this.dict_topic_titles[article_topic].push(_article_title);
+        }
+    }
+
+    insert_data(_dict_title_information) {
+        let idx_topics_arr = 1;
+
+        for (let article_title in _dict_title_information) {
+            let arr_article_topics =
+                _dict_title_information[article_title][idx_topics_arr];
+
+            if (arr_article_topics.length === 0) {
+                console.log('No topics associated.');
+
+                break;
+            }
+
+            this.add_article(article_title, arr_article_topics);
+        }
+    }
+
+    get_articles_by_topics(_arr_topics) {
+        let article_titles = [];
+
+        for (let topic of _arr_topics) {
+            let new_arr_titles = this.dict_topic_titles[topic] || [];
+
+            article_titles = article_titles.concat(new_arr_titles);
+        }
+
+        console.log(
+            '%c Articles Found With the Specified Topics',
+            'color: orange; font-weight: bold;',
+        );
+        console.log({ article_titles });
+
+        /*
+                const matchingArticleIds = [
+            ...articleIds.reduce(
+                (acc, ids) => new Set([...acc].filter(id => ids.has(id))),
+            ),
+        ];
+        */
+
+        return article_titles;
+    }
+}
