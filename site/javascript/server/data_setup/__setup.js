3c2b43e2bf7fce7762a8ea06f5f89345d83210cc
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-09-01 19:08:03 +0200

    Get server data (__setup).

--- site/javascript/server/data_setup/__setup.js
+++ site/javascript/server/data_setup/__setup.js
@@ -1,7 +1,9 @@
 import { Get_Data } from '../server_interaction/get_data.js';
 
+import { Decompress_JSON_File } from './__decompress__/decomp_json.js';
+
 import { ArticlesTrie } from './data_structures/articles_trie.js';
-import { NestedHierarchicalTree } from './data_structures/hierarchical.js';
+import { TopicsInvertedIndex } from './data_structures/inverted_index.js';
 
 async function Setup_Data() {
     let get_topics = Get_Data('data/topics.json', true);
@@ -9,19 +11,33 @@ async function Setup_Data() {
     let bz2_main_dict = await Get_Data('data/main_dict.json', true);
     // let obj_main_dict = Decompress_JSON_File(bz2_main_dict['bz2']);
     let obj_main_dict = bz2_main_dict;
-
     window.trie_articles_data = new ArticlesTrie(obj_main_dict);
 
     let topics = await get_topics;
-    let max_article_topics = 5;
 
-    window.tree_topics_data = new NestedHierarchicalTree(
+    window.inverted_index_topics_title = new TopicsInvertedIndex(
         obj_main_dict,
         topics,
-        max_article_topics,
     );
 
-    console.log(window.tree_topics_data.search_articles(['Politics'], true));
+    console.log(
+        '%c All Articles Information We Get From Server',
+        'color: orange; font-weight: bold;',
+    );
+    console.log({ topics });
+    console.log({ obj_main_dict });
+
+    console.log(
+        '%c Titles Stored With a Prfix Trie Structure',
+        'color: orange; font-weight: bold;',
+    );
+    console.log(window.trie_articles_data);
+
+    console.log(
+        '%c Titles Stored by Topics',
+        'color: orange; font-weight: bold;',
+    );
+    console.log(window.inverted_index_topics_title);
 }
 
 export default Setup_Data;
