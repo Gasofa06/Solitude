da61fda00cf8c0e9766199b76988b97c7e108342
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-09-05 08:51:44 +0200

    OnClick topic suggestion.

---
+++ site/javascript/cross_page_actions/suggestions/search/by_topics/actions/click_suggestion.js
@@ -0,0 +1,30 @@
+import {
+    Clear_Suggestions,
+    Clear_Search_Text_Input,
+    Contains_Obj,
+} from '../../../../../platform_utils/__utils.js';
+
+import { Show_Selected_Topics } from '../constructors/selected_topics.js';
+
+export const Click_Topic_Suggestion = _e => {
+    Clear_Suggestions();
+    Clear_Search_Text_Input();
+
+    window.search_bar.select();
+
+    let clicked_topic = _e.target.getAttribute('data-topic');
+
+    let contains_obj = Contains_Obj(clicked_topic, window.selected_topics);
+
+    if (!contains_obj) {
+        window.selected_topics.push(clicked_topic);
+        // console.log(clicked_topic + " have been added.")
+    } else {
+        window.selected_topics = window.selected_topics.filter(
+            _topic => _topic != clicked_topic,
+        );
+        // console.log(clicked_topic + " have been eliminated.")
+    }
+
+    Show_Selected_Topics();
+};
