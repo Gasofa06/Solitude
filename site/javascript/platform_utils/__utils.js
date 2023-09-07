04f34f614619de8b4373480b35b6c5c5a6b166a8
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-09-07 20:21:58 +0200

    Platform utils main container.

---
+++ site/javascript/platform_utils/__utils.js
@@ -0,0 +1,10 @@
+export * from './action_notifiers/loading_information.js';
+
+export * from './decompression/bz2.js';
+export * from '../server/data_setup/__decompress__/decomp_json.js';
+
+export * from './html_cleaner/clear.js';
+
+export * from './obj_handle/objects.js';
+
+export * from './str_change/title.js';
