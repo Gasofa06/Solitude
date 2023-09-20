eddeda6d05c21b41db529d45e1a22f62558edb5e
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-09-20 23:07:14 +0200

    Global footer with shadow root.

---
+++ site/javascript/components/footer.js
@@ -0,0 +1,42 @@
+const FOOTER = document.createElement('template');
+
+FOOTER.innerHTML = `
+    <link rel="stylesheet" type="text/css" href="css/footer.css" />
+
+    <footer>
+        <div class="footer_container">
+            <a class="logo">incognito dB</a>
+
+            <ul class="footer_links">
+                <li>
+                    <a>Search</a>
+                </li>
+
+                <li>
+                    <a>Documentation</a>
+                </li>
+
+                <li href="https://github.com/Gasofa06/incognito-db">
+                    <a>GitHub</a>
+                </li>
+
+                <li>
+                    <a href="https://twitter.com/rovi_roger">X</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+`;
+
+class Comp__Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.shadow_root = this.attachShadow({ mode: 'closed' });
+        this.shadow_root.appendChild(FOOTER.content);
+    }
+}
+
+customElements.define('comp-footer', Comp__Footer);
