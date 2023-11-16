8396a8a1cc81e837e23260c94a2cbc489aa369d3
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2023-11-16 23:55:36 +0100

    Top nav bar HTML component.

--- src/frontend/javascript/app/components/nav_top_bar.js
+++ src/frontend/javascript/app/components/nav_top_bar.js
@@ -1,42 +1,64 @@
 const NAVIGATION_TOP_BAR = document.createElement('template');
 
 NAVIGATION_TOP_BAR.innerHTML = `
-    <link rel="stylesheet" type="text/css" href="styles/components/nav.css" />
+<link rel="stylesheet" type="text/css" href="styles/components/nav.css" />
 
-    <nav class="top_bar" id="main_container">
-        <div class="nav_content">
-            <div class="left">
-                <a class="logo">incognito dB</a>
-            </div>
+<nav class="nav_top_bar">
+  <div class="nav_top_bar_content">
+    <div class="nav_left">
+      <a class="platform_name">incognito dB</a>
+    </div>
 
-            <div class="right">
-                <ul class="nav_links">
-                    <li id="search">
-                        <a>Search</a>
-                    </li>
-            
-                    <li id="about">
-                        <a>About</a>
-                    </li>
-            
-                    <li id="faqs">
-                        <a>FAQs</a>
-                    </li>
-                </ul>
+    <div class="nav_center">
+      <ul class="nav_links">
+        <li><a>Search</a></li>
 
-                <div class="switch_theme" id="switch_theme_container">
-                    <label class="switch">
-                        <input type="checkbox" id="switch_theme_toggle" />
-              
-                        <div class="slider">
-                            <span>Light</span>
-                            <span>Dark</span>
-                        </div>
-                    </label>
-                </div>
-            </div>
+        <li><a>About</a></li>
+
+        <li><a>Features</a></li>
+
+        <li><a>Write</a></li>
+      </ul>
+    </div>
+
+    <div class="nav_right">
+      <div class="switch_theme" id="switch_theme_container">
+        <label class="switch">
+          <input type="checkbox" id="switch_theme_toggle" />
+
+          <div class="slider">
+            <span>Light</span>
+            <span>Dark</span>
+          </div>
+        </label>
+      </div>
+
+      <div class="lang_dropdown">
+        <div class="lang_dropdown_btn">
+          <div>
+            <svg class="internet_globe" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+              <g>
+                <circle cx="9" cy="9" r="8"></circle>
+                <ellipse cx="9" cy="9" rx="2.8" ry="8"></ellipse>
+                <path d="m1 9h16"></path>
+              </g>
+            </svg>
+          </div>
+
+          <a class="selected_lang">En</a>
         </div>
-    </nav>
+
+        <ul class="lang_menu">
+          <li class="lang_item" data-lang="En"></li>
+          <li class="lang_item" data-lang="En"></li>
+          <li class="lang_item" data-lang="En"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
 `;
 
 class Comp__NavTopBar extends HTMLElement {
