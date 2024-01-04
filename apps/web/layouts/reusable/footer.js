1c03346f643a6cf641da43146a898723e0177892
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2024-01-04 18:08:31 +0100

    Reusable components.

--- apps/web/layouts/reusable/footer.js
+++ apps/web/layouts/reusable/footer.js
@@ -1,7 +1,16 @@
 async function Add_Footer() {
-    const resp = await fetch('footer.html');
-    const html = await resp.text();
-    document.body.insertAdjacentHTML('beforeend', html);
+    const response = await fetch('footer.html');
+
+    if (response.ok) {
+        const html = await response.text();
+        document.body.insertAdjacentHTML('afterend', html);
+
+        return;
+    } else {
+        console.log("Error, the footer couldn't be found.");
+
+        return;
+    }
 }
 
 Add_Footer();
