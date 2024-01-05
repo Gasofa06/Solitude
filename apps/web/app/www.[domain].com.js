128cb0e92bcb66436914cfbec442aaddf92db411
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2024-01-05 12:17:52 +0100

    Animation header in homepage.

--- apps/web/app/www.[domain].com.js
+++ apps/web/app/www.[domain].com.js
@@ -1,1 +1,32 @@
-async function App() {}
+function Main() {
+    Header_Animation();
+}
+
+function Header_Animation() {
+    window.addEventListener(
+        'scroll',
+
+        function (_e) {
+            let section_height =
+                document.getElementById('section-hero').offsetHeight;
+            let hero_content = document.getElementById('hero-content');
+
+            if (this.scrollY + hero_content.offsetHeight < section_height) {
+                hero_content.style.visibility = 'visible';
+            } else {
+                hero_content.style.visibility = 'hidden';
+                return;
+            }
+
+            let scale = (section_height - this.scrollY / 1.6) / section_height;
+            hero_content.style.transform = 'scale(' + scale + ')';
+
+            let opacity = (section_height - this.scrollY) / section_height;
+            hero_content.style.opacity = opacity;
+        },
+
+        false,
+    );
+}
+
+Main();
