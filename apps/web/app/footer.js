2adee543785823fad335e56e4ddc5548e8f85d40
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2024-03-02 17:35:25 +0100

    Footer.

--- apps/web/app/footer.js
+++ apps/web/app/footer.js
@@ -1,5 +1,5 @@
 async function Add_Footer() {
-    const response = await fetch('http://solitude.com/reusable/footer.html');
+    const response = await fetch('http://192.168.1.201/reusable/footer.html');
 
     if (response.ok) {
         const html = await response.text();
